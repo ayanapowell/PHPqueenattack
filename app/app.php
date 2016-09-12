de5f4e70fed77f5737823c174b757c6beaf1baf9
Ayana Powell and HK Kahng <hk@>
2016-09-12 09:35:46 -0700

    create initial files and directories

--- app/app.php
+++ app/app.php
@@ -1,11 +1,12 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/.php";
+    require_once __DIR__."/../src/Pingpong.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
 ));
+
     return $app;
 ?>
